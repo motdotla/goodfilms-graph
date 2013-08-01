bf1fbffd2b04a479f63b1966dd8267a6ca49f8fe
scottmotte <scott@scottmotte.com>
2013-08-01 22:30:08 +0000

    wip

--- test/main.js
+++ test/main.js
@@ -12,4 +12,34 @@ describe('goodfilms-graph', function() {
   it('version should be set', function() {
     result.version.should.eql("0.0.1"); 
   });
+
+  describe('random()', function() {
+    it('should have a film object', function(done) {
+      this.timeout(10000);
+      result.random(function(err, film) {
+        film.title.should.not.be.undefined;
+        console.log('done');
+
+        done();
+      });
+    });
+  });
+
+  describe('_urlExists()', function() {
+    it('should be true for an existing streamer url', function(done) {
+      result._urlExists('http://goodfil.ms/film/61003-the-motorcycle-diaries/netflix_url', function(err, exists) {
+        exists.should.eql(true);
+
+        done();
+      });
+    });
+
+    it('should be false for a non-existing streamer url', function(done) {
+      result._urlExists('http://goodfil.ms/film/61003-the-motorcycle-diaries/hulu_url', function(err, exists) {
+        exists.should.eql(false);
+
+        done();
+      });
+    });
+  });
 });
